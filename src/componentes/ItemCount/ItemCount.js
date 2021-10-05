2ba1219c4f63a0273451f980c3f1585c94d40176
Ernesto Bessone <ejubessone@gmail.com>
2021-10-05 13:53:51 -0300

    desafio 5, ItemCount

--- src/componentes/ItemCount/ItemCount.js
+++ src/componentes/ItemCount/ItemCount.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const ItemCount = ({titulo, texto, imagen}) => {
-    return (   
-        <div>            
-            <div className="row">
-                <div className="col-sm-2">
+const ItemCount = ({titulo, texto, imagen, precio, stock}) => {
+
+    const onAdd = () => {
+        console.log();
+    };
+
+    const [clicks, setClicks] = useState(1)
+
+    console.log(stock)
+
+    const addClick = () => {
+        if (clicks < stock) setClicks (clicks+1)
+    }
+
+    const removeClick = () => {
+        if(clicks > 1) setClicks (clicks-1)
+    }
+
+    return (            
+            <div className="row text-center">
+                <div className="col-2">
                     <div className="card">
                     <img className="card-img-top" src={imagen} alt="producto"/>
                     <div className="card-body">
                         <h5 className="card-title">{titulo}</h5>
-                        <p className="card-text">{texto}</p>
-                        <button className="btn btn-primary">Agregar</button>
-                        <button className="btn btn-primary">Restar</button>
+                        <h4 className="card-text">{texto}</h4>
+                        <p className="card-text">${precio}</p>
+                            <h1>{clicks}</h1>
+                        <div>                      
+                        <button className="btn btn-success m-1" onClick={()=>addClick()}>Agregar</button>
+                        <button className="btn btn-danger m-2" onClick={()=>removeClick()}>Restar</button>
+                        </div>
                     </div>
                     </div>
                 </div>
             </div>
-
-        </div>
     )
 }
 
